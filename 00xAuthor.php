a7bcc0d5d0cf5ad8d16b2c71f765babeb129fcc7
Muraleedharan Koluthappallil <53961270+kolumnin@users.noreply.github.com>
2019-12-27 14:11:53 +0530

    Add files via upload

---
+++ 00xAuthor.php
@@ -0,0 +1,28 @@
+<html><head><title>www_kolumn_in_author_educational_records_<?php echo date("Ymd"); ?></title><LINK TARGET="_blank" REL="Home" HREF="http://www.kolumn.in/Author.php"><LINK  REL="Stylesheet" HREF="http://www.kolumn.in/kolumn.css"><meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<meta content="personal,autobiography" name="keywords">
+<meta content="Autobiographical sketches of Dr.K.Muraleedharan" name="description"></head><body ><div align="center"><header>
+<a href="http://www.kolumn.in"><img alt="" src="http://www.kolumn.in/Images/800x100Header_1.jpg"></a><H1>Welcome to www.kolumn.in</H1>
+</header></div> <hr><nav><p><a href="http://www.kolumn.in/">home</a>&nbsp;<a href="http://www.kolumn.in/login.php">Login</a>&nbsp;<a href="http://www.kolumn.in/about.html">About</a></p></nav><br><H1>Records of My Modern Education</H1><?php include 'Owner.html'; ?><H2>Personal Records</H2><p id="p1">Title: (Mr.) Dr., Individual<br>
+Country of Residence: India<br>
+Country of Birth: India<br>
+Nationality: Indian<br>
+Citizenship: India<br>
+Website:  http://www.kolumn.in<br>
+Name: Muraleedharan<br>
+Father's Name: Eswaran Kesavan Nair<br>
+Mother's Name: N.K.Kamalakshi Amma<br>
+Date of Birth: 17th March 1959<br>Marital Status: Married.</p>
+</div><H2>Academic and Professional Records</H2><div>
+<p id="p3">June 1971- March 1974: Secondary School Leaving Certificate (SSLC). S.C.U. High School, Pattanacaud, Kerala.<br>
+June 1974-March 1976: Pre-Degree with Physics, Chemistry and Mathematics as main subjects under University of Kerala; Studied at St. Michael's College Cherthala, Kerala<br>
+May 1976-March 1979: B.Sc with Physics as Main Subject, under University of Kerala; studied at Sacred Heart College Thevara, Cochin, Kerala<br>
+May 1979-April 1981: M.Sc. Physics Main from Indian Institute of Technology, Kanpur, M.Sc. Thesis on &#39;An Analysis of vibrational spectra of Tutton Salts and the molecular systems MX, M2, X2 and MH (M=H, Li, Na, K, Rb &amp; Cs; X=F, Cl, Br &amp; I) Guided by Pro. H.D.Bist<br>
+June 1981-July 1981: Worked as Junior Research Fellow under Department of Science and Technology Project in Department of Physics, IIT Kanpur<br>
+July 1981-Aug 1986: Research Scholar at Tata Institute of Fundamental Research ,Mumbai. Received Ph.D. degree from the Bombay University for the research work on &quot;Studies on Spin Glass Ordering in some Insulating Spinels&quot; guided by Prof. R.Vijayaraghavan &amp; J.K.Srivastava at Solid State Physics Group<br>
+Sept 1986-March 1987: Post-Doctoral Position with Prof.G.Kaindl, Fachbereich Physik, Freie Universitat, Berlin. Research work on Spin reorientation studies on Nd2Fe14B using Nd Mossbauer Spectroscopy, preparation and studies of High Tc ceramic superconductors<br>
+June 1987-Dec. 1988: Worked as Project Scientist, at Advanced Center for Research in Electronics, Indian Institute of Technology, Powai.<br> Dec. 1988-Aug.1990: Worked as lecturer in Department of Physics at Indian Institute of Technology, Madras.<br>
+Aug. 1993-Jan 1995: Worked as Research associate at Indira Gandhi Center for Atomic Research, Kalpakkam<br></p></div><div>
+<p id="p5">Jan. 1995- Dec.1999: Observing and analyzing the effects of media communications on individuals.<br>Jan. 2000- : Self Analysis, Philosophy, Internet learning<br>Feb. 2014-:  www.kolumn.in owner and developer.</p>
+</div><HR>
+<?php include '8k.php';?>
+<HR></body></html>
